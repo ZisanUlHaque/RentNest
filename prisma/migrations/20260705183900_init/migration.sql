fbeba054382e8b38c96cef5b7c008e827ccad35c
Zisan <mdzisanulhaque989@gmail.com>
2026-07-06 00:46:15 +0600

    Setting Up Multi Prisma Schema File And Defining User Model

---
+++ prisma/migrations/20260705183900_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "ActiveStatus" AS ENUM ('ACTIVE', 'BANNED');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('TENANT', 'LANDLORD', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "activeStatus" "ActiveStatus" NOT NULL DEFAULT 'ACTIVE',
+    "role" "Role" NOT NULL,
+    "profilePhoto" TEXT,
+    "phone" VARCHAR(20) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
